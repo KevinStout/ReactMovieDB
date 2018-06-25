c00e4500ddf1b547626559a16508b820bb2ccbc8
Kevin <stoutkev3@gmail.com>
2018-06-25 03:45:00 -0400

    added styling, also added a transition to go from the main page to the movie detail page

--- moviedb/src/Movie.js
+++ moviedb/src/Movie.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
-  <div>
-    <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
-  </div>
+  <Link to={`/${movie.id}`}>
+    <Overdrive id={movie.id} duration="800">
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Overdrive>
+  </Link>
 );
 
 export default Movie;
@@ -21,6 +23,11 @@ Movie.propTypes = {
     overview: PropTypes.string.isRequired,
   }).isRequired,
 };
+
+export const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+  max-height: 231px;
+`;
 
 // <div>
 // <h3>{movie.title}: {movie.vote_average} out of 10</h3>
--- moviedb/src/MovieDetail.js
+++ moviedb/src/MovieDetail.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import CurrencyFormat from 'react-currency-format';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+import { Poster } from './Movie';
+
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MovieDetail extends Component {
   state = {
@@ -24,35 +30,64 @@ class MovieDetail extends Component {
   render() {
     const { movie } = this.state;
     return (
-      <div>
-        <h1>{this.state.movie.title}</h1>
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-        <h2>{movie.tagline}</h2>
-        <h2>{this.state.movie.release_date}</h2>
-        <p>{this.state.movie.overview}</p>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Budget</th>
-              <th>Popularity</th>
-              <th>Vote Count</th>
-              <th>Average Vote</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{movie.id}</td>
-              <td><CurrencyFormat value={movie.budget} displayType="text" thousandSeparator prefix="$" /></td>
-              <td>{movie.popularity}</td>
-              <td>{movie.vote_count}</td>
-              <td>{movie.vote_average}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title}>
+        <MovieInfo>
+          <Link to="/" >
+            <Overdrive id={movie.id} duration="800">
+              <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+            </Overdrive>
+          </Link>
+          <div>
+            <h1>{movie.title}</h1>
+            <h2>{movie.release_date}</h2>
+            <h2>{movie.tagline}</h2>
+            <p>{movie.overview}</p>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Budget</th>
+                  <th>Popularity</th>
+                  <th>Vote Count</th>
+                  <th>Average Vote</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{movie.id}</td>
+                  <td><CurrencyFormat value={movie.budget} displayType="text" thousandSeparator prefix="$" /></td>
+                  <td>{movie.popularity}</td>
+                  <td>{movie.vote_count}</td>
+                  <td>{movie.vote_average}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
 
 export default MovieDetail;
+
+const MovieWrapper = styled.div`
+    position: relative;
+    padding-top: 70vh;
+    background: url(${props => props.backdrop}) no-repeat;
+    background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+    background: white;
+    text-align: left;
+    padding: 2rem 10%;
+    display: flex;
+    > div{
+        margin-left: 20px;
+    }
+    img{
+        position: relative;
+        top: -5rem;
+    }
+`;
